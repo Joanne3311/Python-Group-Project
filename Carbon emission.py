0d524641c1764b09a8e5aadc9819d29f90eb51b8
Marc-AntoineVinot <159762806+Marc-AntoineVinot@users.noreply.github.com>
2024-04-29 14:24:58 +0200

    Update Carbon emission.py
    update to show other indicators

--- Carbon emission.py
+++ Carbon emission.py
@@ -1,6 +1,7 @@
+#Importation of packages
 import refinitiv.data.eikon as ek
 from IPython.display import HTML
-ek.set_app_key('1131f92f9ebc4ec3870206fdf495a52af08a3795')
+ek.set_app_key('a63d710c9a4f40efa8f37979cb8023d8c26966e4')
 import refinitiv.data as rd
 import numpy as np
 import pandas as pd
@@ -12,18 +13,19 @@ import pandas as pd
 warnings.filterwarnings("ignore")
 
 
-# Datas for the studied stock
-stock_symbol = 'LVMH.PA'  # Replace the value of stock_symbol to get the datas for the stock wanted
+stock_symbol = 'SCHN.PA'  # Replace the value with another refinitiv stock acronym to get the datas for the stock wanted
+# Datas retrieved for the studied stock
 df_single_stock = rd.get_data(stock_symbol, ['TR.TRBCEconomicSector', 'TR.CO2EmissionTotal(Period=FY0)',
-                              'TR.CO2IndirectScope3(Period=FY0)', 'TR.DividendYield'])
+                              'TR.CO2IndirectScope3(Period=FY0)', 'TR.DividendYield','TR.EnvironmentPillarScore(Period=FY0)','TR.TRESGResourceUseScore(Period=FY0)', 
+                              'TR.TRESGEmissionsScore(Period=FY0)','TR.TRESGInnovationScore(Period=FY0)', 'TR.EUTaxTotalEligibleAlignedRevenuePcent(Period=FY0)', 'TR.Tier1GreenRevenuePercentage(Period=FY0)','TR.Tier2GreenRevenuePercentage(Period=FY0)','TR.Tier3GreenRevenuePercentage(Period=FY0)'])
 
 # Columns name
-df_single_stock.columns = ['Instrument', 'Sector',  'CO2Emissions', 'CO2EmissionsScope3',
-               'DividendYield']
+df_single_stock.columns = ['Instrument', 'Sector',  'CO2 Emissions Scope1&2', 'CO2 Emissions Scope3',
+               'Dividend Yield','Environment Score', 'Resource Use Score', 'Emissions Score', 'Environmental Innovation Score', 'EU Taxonomy %Revenue Aligned', 'FTSE Tier 1 Green revenue', 'FTSE Tier 2 Green revenue', 'FTSE tier 3 Green revenue']
 
-# Calculations
-df_single_stock['CO2PerYield'] = (df_single_stock['CO2Emissions'] + df_single_stock['CO2EmissionsScope3'])  / df_single_stock['DividendYield']
-df_single_stock = df_single_stock.round(3)
+# Calculation of the CO2perYield ratio
+df_single_stock['CO2 Per Yield'] = (df_single_stock['CO2 Emissions Scope1&2'] + df_single_stock['CO2 Emissions Scope3'])  / df_single_stock['Dividend Yield']
+df_single_stock = df_single_stock.round(1)
 
 # Plotly package
 fig = go.Figure(data=[go.Table(
@@ -36,7 +38,7 @@ fig = go.Figure(data=[go.Table(
 
 # Width and Height adjusting
 column_widths = [1000] * len(df_single_stock.columns)  # Width
-fig.update_layout(autosize=False, width=1000, height=600)  # Height
+fig.update_layout(autosize=False, width=1500, height=600)  # Height
 fig.update_layout({'xaxis': {'fixedrange': True}})  # fix width
 
 
